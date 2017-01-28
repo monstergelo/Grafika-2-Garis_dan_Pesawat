6bf5a66edfa31aa9dbb75bec7a4800e9ef0ced81
Rio Chandra Rajagukguk <riochr17@gmail.com>
2017-01-28 20:50:03 +0700

    implementasi bufferDrawBot(), refreshBuffer(), loadBuffer() pada gambar.c, fungsi-fungsi pendukung, dan test drivernya

--- gambar.h
+++ gambar.h
@@ -1,34 +1,35 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <linux/fb.h>
-#include <sys/mman.h>
-#include <sys/ioctl.h>
-#include <math.h>
+#ifndef GAMBAR_H
+#define GAMBAR_H
 
-typedef struct{
-    int r,g,b,a;
-} warna;
+// variabel statik global buffer_*
+// tipe data titik, warna
+#include "glob_var_type.h"
 
-typedef struct{
-	int x;
-	int y;
-} titik;
+unsigned char buffer_r[GLOBAL_LAYAR_X][GLOBAL_LAYAR_Y];
+unsigned char buffer_g[GLOBAL_LAYAR_X][GLOBAL_LAYAR_Y];
+unsigned char buffer_b[GLOBAL_LAYAR_X][GLOBAL_LAYAR_Y];
+unsigned char buffer_a[GLOBAL_LAYAR_X][GLOBAL_LAYAR_Y];
 
-//ganti dengan ukuran layar
-unsigned char buffer_r[1366][700];
-unsigned char buffer_g[1366][700];
-unsigned char buffer_b[1366][700];
-unsigned char buffer_a[1366][700];
+// Melakukan assign warna c pada 
+// posisi c pada matriks framebuffer
+void bufferDrawDot(titik p, warna c);
 
-void bufferDrawDot(titik p, warna c);	//mengganti nilai pixel dengan posisi p pada buffer dengan warna c
-void refreshBuffer(titik p0, titik p1); //mengganti nilai seluruh pixel buffer menjadi background color untuk
-void loadBuffer();						//memasukkan nilai buffer ke driver
+// Melakukan assign warna default ke matriks 
+// buffer (bukan matriks framebuffer) berupa
+// segi empat yang dibentuk oleh dua titik p0 dan p1
+void refreshBuffer(titik p0, titik p1);
+
+// Memuat/assign nilai dari matriks buffer
+// ke matriks framebuffer.
+void loadBuffer();
+
+
 //================================================
 void bufferDrawLine(titik p0, titik p1, warna c);	//mmengganti nilai pixel sehingga tergambar garis dari p0 ke p1
-void bufferDrawPlane(titik* p, , warna c);			//membuat bidang yang menyambungkan titik p[0] ke p[1], p[1] ke p[2], ...., p[n] ke p[0]
+void bufferDrawPlane(titik* p, warna c);			//membuat bidang yang menyambungkan titik p[0] ke p[1], p[1] ke p[2], ...., p[n] ke p[0]
 //================================================
 void bufferDrawCircle(titik p, warna c);	//membuat lingkaran dengan p sebagai pusat
 int  dotDistance(titik p1, titik p2);		//mengembalikan jarak antara p1 dan p2
 //================================================
+
+#endif //GAMBAR_H
